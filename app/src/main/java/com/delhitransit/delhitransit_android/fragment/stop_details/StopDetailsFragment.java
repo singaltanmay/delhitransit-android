72644b0c0b60d9ccf575792071a73bb7b5ff81b2
singaltanmay <tanmaysingal@icloud.com>
2020-12-17 00:49:17 +0530

    Open Route details after clicking on a route detail in StopDetailsFragment
    Signed-off-by: Tanmay Singal <tanmaysingal@icloud.com>

--- app/src/main/java/com/delhitransit/delhitransit_android/fragment/stop_details/StopDetailsFragment.java
+++ app/src/main/java/com/delhitransit/delhitransit_android/fragment/stop_details/StopDetailsFragment.java
@@ -1,7 +1,9 @@
 package com.delhitransit.delhitransit_android.fragment.stop_details;
 
+import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,9 +20,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.delhitransit.delhitransit_android.R;
 import com.delhitransit.delhitransit_android.adapter.StopDetailsAdapter;
 import com.delhitransit.delhitransit_android.interfaces.FragmentFinisherInterface;
+import com.delhitransit.delhitransit_android.interfaces.OnRouteDetailsSelectedListener;
+import com.delhitransit.delhitransit_android.pojos.route.RoutesFromStopDetail;
 import com.delhitransit.delhitransit_android.pojos.stops.StopDetail;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+
+import java.util.function.Consumer;
 
 public class StopDetailsFragment extends Fragment {
 
@@ -31,6 +37,7 @@ public class StopDetailsFragment extends Fragment {
     private StopDetailsViewModel mViewModel;
     private StopDetailsAdapter adapter;
     private MaterialToolbar toolbar;
+    private Activity activity;
 
     public StopDetailsFragment(StopDetail stop, Runnable fabClickCallback) {
         this.stop = stop;
@@ -41,13 +48,21 @@ public class StopDetailsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View parent = inflater.inflate(R.layout.stop_details_fragment, container, false);
+        activity = getActivity();
         //Setup the toolbar
         toolbar = parent.findViewById(R.id.stop_details_fragment_app_bar);
         toolbar.setTitle(stop.getName());
         toolbar.setNavigationOnClickListener(item -> finishMe(null));
         //Setup the recycler view
         RecyclerView recyclerView = parent.findViewById(R.id.stop_details_fragment_recycler_view);
-        adapter = new StopDetailsAdapter();
+        Consumer<RoutesFromStopDetail> consumer = routesFromStopDetail -> {
+            if (activity instanceof OnRouteDetailsSelectedListener) {
+                ((OnRouteDetailsSelectedListener) activity).onRouteSelect(routesFromStopDetail, stop);
+            } else {
+                Log.e(StopDetailsFragment.this.getClass().getSimpleName(), "Calling class does not implement " + OnRouteDetailsSelectedListener.class.getSimpleName());
+            }
+        };
+        adapter = new StopDetailsAdapter(consumer);
         recyclerView.setAdapter(adapter);
         recyclerView.setMinimumHeight(Resources.getSystem().getDisplayMetrics().heightPixels);
         //Setup the navigate FAB
@@ -110,9 +125,8 @@ public class StopDetailsFragment extends Fragment {
     }
 
     public void finishMe(Runnable callback) {
-        FragmentFinisherInterface activity = (FragmentFinisherInterface) getActivity();
-        if (activity != null) {
-            activity.finishAndExecute(KEY_FRAGMENT_BACKSTACK, callback == null ? () -> {
+        if (activity instanceof FragmentFinisherInterface) {
+            ((FragmentFinisherInterface) activity).finishAndExecute(KEY_FRAGMENT_BACKSTACK, callback == null ? () -> {
             } : callback);
         }
     }
